f4a6cff4f3f699d61cf9e458e22c6d10a02506c2
uevertonmenezes <174225136+uevertonmenezes@users.noreply.github.com>
2025-09-28 15:05:36 -0300

    adicionando calculadora de horas extras

--- calculadoras_trabalhistas/calculadora_ferias.py
+++ calculadoras_trabalhistas/calculadora_ferias.py
@@ -3,7 +3,7 @@ print('**Calculadora de Férias**')
 
 salario_bruto = float(input("Qual o seu salário bruto?"))
 ferias_bruto = salario_bruto + (salario_bruto / 3)
-valor_descontado = ferias_bruto * (9 / 100)
+valor_descontado = ferias_bruto * (9 / 100) #definir alíquotas e descontos
 
 remuneracao_ferias = ferias_bruto - valor_descontado
 print("O total a receber das suas férias é", remuneracao_ferias)
---
+++ calculadoras_trabalhistas/calculadora_horasExtras.py
@@ -0,0 +1,8 @@
+print('**Calculadora de Horas Extras**')
+
+salario_bruto = int(input("Qual o seu salário bruto?"))
+
+hora_normal = salario_bruto / 220
+hora_extra = hora_normal / 0.5
+
+print('O seu valor por cada hora extra é de', hora_extra)
