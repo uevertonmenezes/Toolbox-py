e0be95e511da71ecd85e7b92eee98f425d93f920
uevertonmenezes <174225136+uevertonmenezes@users.noreply.github.com>
2026-01-12 21:39:43 -0300

    Atualização inicial calculadora de décimo terceiro

--- calculadoras_trabalhistas/calculadora_decimoTerceiro.py
+++ calculadoras_trabalhistas/calculadora_decimoTerceiro.py
@@ -1,10 +1,22 @@
-print("**Calculadora de décimo terceiro**")
+print("**Calculadora de Décimo Terceiro**")
 
-salario_bruto = float(input("Qual o seu salário bruto?"))
-
-meses_trabalhados = int(input("Quantos meses você trabalhou esse ano?"))
+salario_bruto = float(input("Qual o seu salário bruto? (Sem os descontos)\n"))
+meses_trabalhados = int(input("Quantos meses você trabalhou esse ano?\n"))
+parcela_decimo = float(input(
+    "Qual parcela deseja simular?\n"
+        "0- Única\n"
+        "1- Primeira Parcela\n"
+        "2- Segunda Parcela\n"))
 
 salario_mes = salario_bruto / 12
-decimo_terceiro = salario_mes * meses_trabalhados
 
-print('o valor do seu décimo terceiro é de', decimo_terceiro)
+if parcela_decimo == 0:
+    decimo_terceiro = (salario_mes * meses_trabalhados) * (91 / 100) #alíquota de teste (9%), logo mais será adicionado tabela real de alíquotas
+
+elif parcela_decimo == 1:
+    decimo_terceiro = (salario_mes * meses_trabalhados) / 2
+
+else:
+    decimo_terceiro = (salario_mes * meses_trabalhados) / 2 * (91 / 100)
+
+print(f'o valor do seu décimo terceiro é de: R${decimo_terceiro:.2f}')
