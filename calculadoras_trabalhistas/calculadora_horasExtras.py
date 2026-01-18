385ac76375144da6dec9d80585a0faa46819cb42
uevertonmenezes <174225136+uevertonmenezes@users.noreply.github.com>
2026-01-18 18:08:30 -0300

    Atualização calculadora de horas extras

--- calculadoras_trabalhistas/calculadora_horasExtras.py
+++ calculadoras_trabalhistas/calculadora_horasExtras.py
@@ -2,18 +2,31 @@ print('**Calculadora de Horas Extras**\n')
 
 salario_bruto = float(input("Qual o seu salário bruto? (Sem os descontos)\n"))
 hora_mes = float(input("Qual a sua carga horária mensal? (Considere 220h para uma jornada de 44h semanais)\n"))
-porcentagem_hora = float(input("Qual a porcentagem da hora? (Considere 50% para dias comuns e 100% para domingos e feriados)\n"))
-horas_trabalhadas = float(input("Qual a quantidade de horas extras trabalhadas?\n"))
 
-hora_normal = salario_bruto / hora_mes
+hora_extra50 = (input("Qual a quantidade de horas extras normais trabalhadas? (Digite no formato HH:MM)\n"))
+hora_extra100 = (input("Qual a quantidade de horas extras trabalhadas nos domingos e feriados (100%)? (Digite no formato HH:MM)\n"))
+hora_noturna = (input("Qual a quantidade de horas noturnas trabalhadas? (Digite no formato HH:MM)\n"))
 
-if porcentagem_hora == 50:
-    hora_extra = hora_normal + (hora_normal * 0.5)
-else:
-    hora_extra = hora_normal + (hora_normal * 1)
+def converter_hora(hora):
+    h, m = map(int, hora.split(':'))
+    return h * 60 + m
 
-hora_total = hora_extra * horas_trabalhadas
+min_extra50 = converter_hora(hora_extra50)
+min_extra100 = converter_hora(hora_extra100)
+min_noturna = converter_hora(hora_noturna)
 
-print(f'Valor da hora normal é: R${hora_normal:.2f}')
-print(f'O valor por cada hora extra é de: R${hora_extra:.2f}')
-print(f'O valor total pelas horas extras trabalhadas é de: R${hora_total:.2f}')
+valor_hora = salario_bruto / hora_mes
+valor_minuto = valor_hora / 60
+
+valor_extra50 = min_extra50 * valor_minuto * 1.5
+valor_extra100 = min_extra100 * valor_minuto * 2
+valor_noturna = (min_noturna * valor_minuto * 1.5) * 1.20
+
+total = valor_extra50 + valor_extra100 + valor_noturna
+
+print('\n**Resultado do Cálculo**\n')
+print(f'Valor da hora trabalhada é: R${valor_hora:.2f}')
+print(f'Valor da hora extra normal é: R${valor_extra50:.2f}')
+print(f'Valor da hora extra 100% é: R${valor_extra100:.2f}')
+print(f'Valor da hora noturna trabalhada é: R${valor_noturna:.2f}')
+print(f'Valor total: R${total:.2f}')
